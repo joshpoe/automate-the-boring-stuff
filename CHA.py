fa182a9b625e641cfd7299ff68dbb1b35636d969
Derek Shoemaker <shoemakerdr@gmail.com>
2018-10-10 20:16:20 -0500

    Added name checking logic

--- CHA.py
+++ CHA.py
@@ -1,7 +1,32 @@
 # First fun program
 
+#######################
+# Users
+#######################
+JOSH = 'Josh'
+DEREK = 'Derek'
+
+
+#######################
+# Python conventions
+#######################
+CONSTANTS = 'Never going to change'
+this_is_a_variable = 'Something that could change'
+
+
+def is_same_name(name1, name2):
+    return name1.lower() == name2.lower()
+
+
 print ('Hello, please enter your company provided user ID')
 userID = input()
-print ('Hello, ' + userID) 
-if userID == (Josh) then print ('Hello')
-	
+if len(userID) > 0:
+    print ('Hello, ' + userID) 
+    if is_same_name(userID, DEREK):
+        print ('Welcome back. You are a good programmer.')
+    elif is_same_name(userID, JOSH):
+        print ('Welcome back. You will be a good programmer very soon.')
+    else:
+        print("Um... I don't know you dude")
+else:
+    print('You need to tell me your damn USER ID!')
